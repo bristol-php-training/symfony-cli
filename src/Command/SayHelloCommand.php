d4a28020cd924dc2fee271d4aea3047512f24631
Dave Liddament <dave@lampbristol.com>
2017-08-02 14:33:59 +0100

    ADD say hello command

---
+++ src/Command/SayHelloCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace BristolPhpTraining\cli\Command;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SayHelloCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('say:hello');
+        $this->setDescription("Says hello");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("Hello Bristol PHP Training");
+    }
+
+
+}
