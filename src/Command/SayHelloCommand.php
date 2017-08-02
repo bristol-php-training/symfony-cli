872d69b99cf97c959e06804df499a2232c3505bf
Dave Liddament <dave@lampbristol.com>
2017-08-02 16:08:02 +0100

    ADD flag option

--- src/Command/SayHelloCommand.php
+++ src/Command/SayHelloCommand.php
@@ -7,6 +7,7 @@ namespace BristolPhpTraining\cli\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SayHelloCommand extends Command
@@ -17,13 +18,18 @@ class SayHelloCommand extends Command
         $this->setDescription("Says hello");
         $this->addArgument('first', InputArgument::REQUIRED, 'First anme');
         $this->addArgument('last', InputArgument::REQUIRED, 'Last anme');
+        $this->addOption('compliment', 'c', InputOption::VALUE_NONE, 'Add a compliment');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $firstName = $input->getArgument('first');
         $lastName = $input->getArgument('last');
-        $output->writeln("Hello {$firstName} {$lastName}");
+        $message = "Hello {$firstName} {$lastName}.";
+        if ($input->getOption('compliment')) {
+            $message .= " You're looking good today";
+        }
+        $output->writeln($message);
     }
 
 }
